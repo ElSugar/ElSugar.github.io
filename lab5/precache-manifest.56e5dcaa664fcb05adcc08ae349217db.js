8508d8fcb2edb0f15d925465fe85d80fe58c155e
Elizabeth Sakharova <elizabeth.saharova@mail.ru>
2020-05-13 19:08:51 +0300

    Add files via upload

---
+++ lab5/precache-manifest.56e5dcaa664fcb05adcc08ae349217db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc2eb1ce1932e096573e6253b1036aa5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "486cd0efe3e76a6ca4f2",
+    "url": "/static/css/main.47ec4ba6.chunk.css"
+  },
+  {
+    "revision": "ef12f83eaf1458b6fe8d",
+    "url": "/static/js/2.c130534a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c130534a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "486cd0efe3e76a6ca4f2",
+    "url": "/static/js/main.43956919.chunk.js"
+  },
+  {
+    "revision": "b221a071806db990eb19",
+    "url": "/static/js/runtime-main.85dac6e8.js"
+  }
+]);
